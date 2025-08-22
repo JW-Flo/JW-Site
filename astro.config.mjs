f730c31cd0bdfae53ba11bd4626fbe905e855e9d
JW <jw@JWs-MacBook-Pro.local>
2025-08-21 22:37:24 -0500

    chore: set canonical site URL to match repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://JW-Flo.github.io/jw-site',
+  // Canonical site URL used by Astro for absolute links & sitemap (match repo name casing)
+  site: 'https://jw-flo.github.io/JW-Site',
   integrations: [tailwind(), mdx()],
 });
