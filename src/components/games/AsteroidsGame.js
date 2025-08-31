bf0997f20cc8e19a8c533831aa6c9f5fb6ae5922
JW-Flo <jw@example.com>
2025-08-31 12:51:41 -0500

    chore: disable SCANNER_META KV binding placeholder + add RAF fallback guards for game loops

--- src/components/games/AsteroidsGame.js
+++ src/components/games/AsteroidsGame.js
@@ -559,7 +559,10 @@ export class AsteroidsGame {
   gameLoop() {
     this.update();
     this.draw();
-    this.animationId = requestAnimationFrame(() => this.gameLoop());
+    const raf = (typeof requestAnimationFrame !== 'undefined')
+      ? requestAnimationFrame
+      : (cb) => setTimeout(cb, 16);
+    this.animationId = raf(() => this.gameLoop());
   }
 
   draw() {
